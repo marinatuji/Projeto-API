9c8914acbfb8dc33156bb392260299a40e5faff4
marinatuji <mtujimura@gmail.com>
2019-05-22 03:49:18 -0300

    API request implementada para selecionar estados e municipios

--- src/home.js
+++ src/home.js
@@ -1,18 +1,48 @@
 const endpoint = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
-const selectState = document.querySelector("#api-states");
+// const endpoint = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/municipios';
+const checkLocation = document.querySelector("#search-check-location");
+const selectState = document.querySelector('#search-select-state');
+const checkOnline = document.querySelector('#search-check-online');
 
-selectState.addEventListener("click", function (eve) {
+checkLocation.addEventListener('change', function (eve) {
+  if(this.checked){
   eve.preventDefault();
   fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
-      //const UF = getData.id
-      const getResult = document.querySelector(".results-list");//armazenei dom
-      const createUL = document.createElement("article");;
-      const createText = document.createTextNode(data);
-      createUL.appendChild(createText);
-      getResult.appendChild(createUL);
-      // console.log(resp);
+      const select = document.querySelector('#search-select-state');
+      select.length = 0;
+      data
+        .sort((a, b) => {
+          if (a.sigla < b.sigla) {
+            return -1;
+          } if (a.sigla > b.sigla) {
+            return 1;
+          }
+          return 0;
+        })
+        .map((state) => {
+          select.options.add(new Option(state.sigla, state.id));
+        });
+    })
+    .catch((resp) => {
+      console.error(resp);
+    });
+  }
+});
+
+selectState.addEventListener('change', function (eve) {
+  eve.preventDefault();
+  const statesID = selectState.options[selectState.selectedIndex].value;
+  const endpoint = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${statesID}/municipios`;
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      const select = document.querySelector('#search-select-municipality');
+      select.length = 0;
+      data.map((munic) => {
+        select.options.add(new Option(munic.nome, munic.id))
+      });
     })
     .catch((resp) => {
       console.error(resp);
